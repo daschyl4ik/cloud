3456a9e452314e950a263130ca88b9bf294d78fc
daschyl4ik <darya.kolganova1@gmail.com>
2023-07-25 21:32:18 +0800

    Beginning
    Login
    Register

---
+++ myapp/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField("Email: ", validators=[Email("Некорректный email")])
+    psw = PasswordField("Пароль:", validators=[DataRequired(), Length(min=4, max=20, message = "Пароль должен содержать от 4 до 20 символов")])
+    remember = BooleanField("Запомнить", default = False)
+    submit = SubmitField("Войти")
+
+class RegisterForm(FlaskForm):
+    name = StringField("Имя:", validators=[DataRequired(), Length(min=0, max=20, message = "Введите имя длиной до 20 символов")])
+    email = StringField("Email: ", validators=[Email("Некорректный email")])
+    psw = PasswordField("Пароль:", validators=[DataRequired(), Length(min=4, max=20, message = "Пароль должен содержать от 4 до 20 символов")])
+    psw2 = PasswordField("Повтор пароля:", validators=[DataRequired(), EqualTo("psw", message = "Пароли не совпадают")])
+    submit = SubmitField("Зарегистрироваться")
